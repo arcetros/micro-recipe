b8f8cde89e92bf785c704a78d716f0c9819296e1
arcetros <75669794+arcetros@users.noreply.github.com>
2022-10-11 22:42:52 +0800

    feat(scraper): convert from class to function

---
+++ src/helpers/domains/index.js
@@ -0,0 +1,14 @@
+const domains = ["masakapahariini", "stryve"]
+
+const selectors = {
+  stryve: {
+    titleSelector: "h1",
+    ingredientSelector: "ul[class='IngredientList_list__icAZR'] > li",
+    _ingredientQty: ".Ingredient_amount__YdGKo",
+    _ingredientName: ".Ingredient_name__ZXffJ",
+    directionsSelector: "ol > li p",
+    authorSelector: "meta[name='twitter:creator']",
+  },
+}
+
+module.exports = { domains, selectors }
