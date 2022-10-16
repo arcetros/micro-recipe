c8805331dafad2c1c17f73008339c8aca506668b
arcetros <75669794+arcetros@users.noreply.github.com>
2022-10-16 15:17:07 +0800

    chore(AlternativeScraper): add try catch

--- src/helpers/AlternativeScraper.js
+++ src/helpers/AlternativeScraper.js
@@ -29,122 +29,123 @@ function getResponseAlt(html) {
       json.children.forEach((el) => {
         if (el.data) {
           const jsonRaw = el.data
-          const result = JSON.parse(jsonRaw)
-          let recipe
+          try {
+            const result = JSON.parse(jsonRaw)
+            let recipe
 
-          if (result["@graph"] && Array.isArray(result["@graph"])) {
-            result["@graph"].forEach((g) => {
-              if (g["@type"] === "Recipe") {
-                recipe = g
-              }
-            })
-          }
-
-          if (result["@type"] === "Recipe") {
-            recipe = result
-          }
-
-          if (
-            Array.isArray(result["@type"]) &&
-            result["@type"].includes("Recipe")
-          ) {
-            recipe = result
-          }
-
-          if (recipe) {
-            try {
-              _recipe.name = decodeHTML(html, recipe.name)
-
-              if (recipe.description) {
-                _recipe.description = decodeHTML(html, recipe.description)
-              } else {
-                getDescriptionFromSelector(html)
-              }
-
-              if (Array.isArray(recipe.image)) {
-                recipe.image = recipe.image[0]
-              }
-
-              if (recipe.image) {
-                if (
-                  recipe.image["@type"] === "ImageObject" &&
-                  recipe.image.url
-                ) {
-                  _recipe.image = recipe.image.url
-                } else if (typeof recipe.image === "string") {
-                  _recipe.image = recipe.image
+            if (result["@graph"] && Array.isArray(result["@graph"])) {
+              result["@graph"].forEach((g) => {
+                if (g["@type"] === "Recipe") {
+                  recipe = g
                 }
-              } else {
-                getImageFromSelector(html)
-              }
-
-              if (Array.isArray(recipe.recipeIngredient)) {
-                _recipe.ingredients = recipe.recipeIngredient.map((i) =>
-                  decodeHTML(html, i)
-                )
-              } else if (typeof recipe.recipeIngredient === "string") {
-                _recipe.ingredients = recipe.recipeIngredient
-                  .split(",")
-                  .map((i) => decodeHTML(html, i.trim()))
-              }
-
-              if (
-                recipe.recipeInstructions &&
-                recipe.recipeInstructions["@type"] === "ItemList" &&
-                recipe.recipeInstructions.itemListElement
-              ) {
-                recipe.recipeInstructions.itemListElement.forEach((section) => {
-                  _recipe.instructions = [
-                    ..._recipe.instructions,
-                    ...section.itemListElement.map((i) =>
-                      decodeHTML(html, i.text)
-                    ),
-                  ]
-                  section.itemListElement.forEach((i) => {
-                    _recipe.sectionedInstructions.push({
-                      sectionTitle: section.name,
-                      text: decodeHTML(html, i.text),
-                      image: i.image || "",
-                    })
-                  })
-                })
-              } else if (Array.isArray(recipe.recipeInstructions)) {
-                recipe.recipeInstructions.forEach((instructionStep) => {
-                  if (instructionStep["@type"] === "HowToStep") {
-                    _recipe.instructions.push(
-                      decodeHTML(html, instructionStep.text)
-                    )
-                  } else if (typeof instructionStep === "string") {
-                    _recipe.instructions.push(decodeHTML(html, instructionStep))
-                  } else if (typeof recipe.recipeInstructions === "string") {
-                    _recipe.instructions = [
-                      decodeHTML(html, recipe.recipeInstructions),
-                    ]
-                  }
-                })
-              }
-              if (recipe.prepTime) {
-                _recipe.time.prep = parsePTTime(recipe.prepTime)
-              }
-
-              if (recipe.cookTime) {
-                _recipe.time.cook = parsePTTime(recipe.cookTime)
-              }
-
-              if (recipe.totalTime) {
-                _recipe.time.total = parsePTTime(recipe.totalTime)
-              }
-
-              if (Array.isArray(recipe.recipeYield)) {
-                _recipe.servings = recipe.recipeYield[0]
-              } else if (typeof recipe.recipeYield === "string") {
-                _recipe.servings = recipe.recipeYield
-              }
-              _recipe.servings = recipe.recipeYield.toString()
-              _recipe.author = recipe.author.name
-            } catch (err) {
-              throw new Error("Site is not yet supported")
+              })
             }
+
+            if (result["@type"] === "Recipe") {
+              recipe = result
+            }
+
+            if (
+              Array.isArray(result["@type"]) &&
+              result["@type"].includes("Recipe")
+            ) {
+              recipe = result
+            }
+
+            if (recipe) {
+              try {
+                _recipe.name = decodeHTML(html, recipe.name)
+
+                if (recipe.description) {
+                  _recipe.description = decodeHTML(html, recipe.description)
+                } else {
+                  getDescriptionFromSelector(html)
+                }
+
+                if (Array.isArray(recipe.image)) {
+                  recipe.image = recipe.image[0]
+                }
+
+                if (recipe.image) {
+                  if (
+                    recipe.image["@type"] === "ImageObject" &&
+                    recipe.image.url
+                  ) {
+                    _recipe.image = recipe.image.url
+                  } else if (typeof recipe.image === "string") {
+                    _recipe.image = recipe.image
+                  }
+                } else {
+                  getImageFromSelector(html)
+                }
+
+                if (Array.isArray(recipe.recipeIngredient)) {
+                  _recipe.ingredients = recipe.recipeIngredient.map((i) =>
+                    decodeHTML(html, i)
+                  )
+                } else if (typeof recipe.recipeIngredient === "string") {
+                  _recipe.ingredients = recipe.recipeIngredient
+                    .split(",")
+                    .map((i) => decodeHTML(html, i.trim()))
+                }
+
+                if (
+                  recipe.recipeInstructions &&
+                  recipe.recipeInstructions["@type"] === "ItemList" &&
+                  recipe.recipeInstructions.itemListElement
+                ) {
+                  recipe.recipeInstructions.itemListElement.forEach(
+                    (section) => {
+                      _recipe.instructions = [
+                        ..._recipe.instructions,
+                        ...section.itemListElement.map((i) =>
+                          decodeHTML(html, i.text)
+                        ),
+                      ]
+                    }
+                  )
+                } else if (Array.isArray(recipe.recipeInstructions)) {
+                  recipe.recipeInstructions.forEach((instructionStep) => {
+                    if (instructionStep["@type"] === "HowToStep") {
+                      _recipe.instructions.push(
+                        decodeHTML(html, instructionStep.text)
+                      )
+                    } else if (typeof instructionStep === "string") {
+                      _recipe.instructions.push(
+                        decodeHTML(html, instructionStep)
+                      )
+                    } else if (typeof recipe.recipeInstructions === "string") {
+                      _recipe.instructions = [
+                        decodeHTML(html, recipe.recipeInstructions),
+                      ]
+                    }
+                  })
+                }
+                if (recipe.prepTime) {
+                  _recipe.time.prep = parsePTTime(recipe.prepTime)
+                }
+
+                if (recipe.cookTime) {
+                  _recipe.time.cook = parsePTTime(recipe.cookTime)
+                }
+
+                if (recipe.totalTime) {
+                  _recipe.time.total = parsePTTime(recipe.totalTime)
+                }
+
+                if (Array.isArray(recipe.recipeYield)) {
+                  _recipe.servings = recipe.recipeYield[0]
+                } else if (typeof recipe.recipeYield === "string") {
+                  _recipe.servings = recipe.recipeYield
+                }
+                _recipe.servings = recipe.recipeYield.toString()
+                _recipe.author = recipe.author.name
+              } catch (err) {
+                throw new Error("Something went wrong")
+              }
+            }
+          } catch (err) {
+            throw new Error("Site is not yet supported")
           }
         }
       })
